53b7dacf36f8e5f6ae3d6aca1b296527232ef111
Gary Mardell <info@garymardell.co.uk>
2025-04-29 17:30:46 -0700

    Make pool options a setting

--- src/clerk.cr
+++ src/clerk.cr
@@ -7,12 +7,13 @@ module Clerk
   Habitat.create do
     setting url : String = "https://api.clerk.com"
     setting token : String
+    setting pool_options : DB::Pool::Options = DB::Pool::Options.new(initial_pool_size: 0, max_idle_pool_size: 25)
   end
 
   @@pool : DB::Pool(HTTP::Client)? = nil
 
   def self.pool : DB::Pool(HTTP::Client)
-    @@pool ||= DB::Pool.new(initial_pool_size: 0, max_idle_pool_size: 25) {
+    @@pool ||= DB::Pool.new(Clerk.settings.pool_options) {
       client = HTTP::Client.new(URI.parse(Clerk.settings.url))
 
       client.before_request do |request|
